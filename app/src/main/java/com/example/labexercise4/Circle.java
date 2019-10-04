29351effa9047d443e2e40194ffcbdaf0faab7c0
RidwanAndalib <andalib369@gmail.com>
2019-10-04 13:41:51 -0600

    color added to master

--- app/src/main/java/com/example/labexercise4/Circle.java
+++ app/src/main/java/com/example/labexercise4/Circle.java
@@ -1,4 +1,12 @@
 package com.example.labexercise4;
 
-public class Circle {
+public class Circle extends Shape {
+
+    public Circle(int x, int y){
+        super(x,y);
+    }
+
+    public Circle(){
+        super();
+    }
 }
